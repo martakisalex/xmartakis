0aff31887c2d966f39583521954a2a783b14b6ce
martakisalex <martakisalex@gmail.com>
2024-08-09 21:11:30 -0700

    refactor: replace axios with fetch
    - update googleSheetsApi.js

--- src/utils/googleSheetsApi.js
+++ src/utils/googleSheetsApi.js
@@ -1,24 +1,25 @@
-import axios from 'axios';
-
 // Accessing environment variables using import.meta.env
 const sheetId = import.meta.env.VITE_GOOGLE_SHEET_ID;
 const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 
-console.log(sheetId)
-console.log(apiKey)
+console.log(sheetId);
+console.log(apiKey);
 
 /**
- * Fetch data from a specific sheet and range.
+ * Fetch all data from a specific sheet.
  * @param {string} sheetName - The name of the sheet/tab.
- * @param {string} range - The range of cells to fetch.
  * @returns {Promise<Array>} - Promise that resolves to an array of rows.
  */
-export const fetchSheetData = async (sheetName, range) => {
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}!${range}?key=${apiKey}`;
+export const fetchSheetData = async (sheetName) => {
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}?key=${apiKey}`;
 
   try {
-    const response = await axios.get(url);
-    return response.data.values || [];
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.values.slice(1) || []; // Slice to remove header row if present
   } catch (error) {
     console.error(`Error fetching data from ${sheetName}:`, error);
     return [];

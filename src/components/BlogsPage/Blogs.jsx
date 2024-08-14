c6917128ddd5fc4f274891f95765ae5e33f759c6
martakisalex <martakisalex@gmail.com>
2024-08-13 22:41:46 -0700

    wip2

--- src/components/BlogsPage/Blogs.jsx
+++ src/components/BlogsPage/Blogs.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { fetchSheetData } from '../../utils/googleSheetsApi';
 import './Blogs.css';
-import { Link } from 'react-router-dom';
 
 function Blogs() {
   const [blogs, setBlogs] = useState([]);
@@ -10,11 +9,10 @@ function Blogs() {
     const fetchBlogs = async () => {
       try {
         const data = await fetchSheetData('blogs');
-        setBlogs(data.map(([title, date, imageUrl, content, excerpt]) => ({
+        setBlogs(data.map(([title, date, imageUrl, excerpt]) => ({
           title,
           date,
           imageUrl,
-          content,
           excerpt,
         })));
       } catch (error) {
@@ -25,26 +23,18 @@ function Blogs() {
     fetchBlogs();
   }, []);
 
-  const truncateContent = (content, length) => {
-    if (content.length > length) {
-      return content.substring(0, length) + '... Read more';
-    }
-    return content;
-  };
-
   return (
     <div className="blogs-container">
       {blogs.map((blog, index) => (
-        <Link to={`/blogs/${blog.title.toLowerCase().replace(/\s+/g, '-')}`} key={index} className="blog-entry-link">
-          <div className="blog-entry">
+        <div key={index} className="blog-entry">
+          <div className="blog-image-container">
             <img src={blog.imageUrl} alt={blog.title} className="blog-image" />
-            <div className="blog-content">
+            <div className="blog-content-overlay">
               <h3 className="blog-title">{blog.title}</h3>
               <p className="blog-excerpt">{blog.excerpt}</p>
-              <p className="blog-preview">{truncateContent(blog.content, 150)}</p>
             </div>
           </div>
-        </Link>
+        </div>
       ))}
     </div>
   );
